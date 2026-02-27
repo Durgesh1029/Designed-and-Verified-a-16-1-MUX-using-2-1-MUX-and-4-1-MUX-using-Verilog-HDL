0a8eb82e1cc6c426676fed0075bde6682bbf1a1f
Durgesh Dongre <67177944+Durgesh1029@users.noreply.github.com>
2026-02-27 08:28:51 +0530

    Update mux16_1_st.v

--- mux16_1_st.v
+++ mux16_1_st.v
@@ -1,12 +1,13 @@
 module mux16_1_st(in,s,y);
-input [15:0]in;
-input[3:0]s;
+input [15:0] in;
+input [3:0] s;
 output y;
-wire [3:0]t;
-mux4_1_st mux4x1_1(in[3:0],s[1:0],t[0]);
-mux4_1_st mux4x1_2(in[7:4],s[1:0],t[1]);
-mux4_1_st mux4x1_3(in[11:8],s[1:0],t[2]);
-mux4_1_st mux4x1_4(in[15:12],s[1:0],t[3]);
-mux4_1_st mux4x1_5(t,s[3:2],y);
+wire [3:0] t;
+// First stage uses lower 2 bits [1:0]
+mux4_1_st mux_4_st_1(in[3:0],s[1:0],t[0]);
+mux4_1_st mux_4_st_2(in[7:4],s[1:0],t[1]);
+mux4_1_st mux_4_st_3(in[11:8],s[1:0],t[2]);
+mux4_1_st mux_4_st_4(in[15:12],s[1:0],t[3]);
+// Final stage uses upper 2 bits [3:2]
+mux4_1_st mux_4_st_5(t,s[3:2],y);
 endmodule
-
