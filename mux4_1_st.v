b630b32b1861c4e13bfd7fc928ae4b7ff5f979a4
Durgesh Dongre <67177944+Durgesh1029@users.noreply.github.com>
2026-02-27 08:27:46 +0530

    Add comments for clarity in mux4_1_st module
    Added comments to clarify the stages of the multiplexer.

--- mux4_1_st.v
+++ mux4_1_st.v
@@ -1,9 +1,11 @@
 module mux4_1_st(in,s,y);
-input [3:0]in;
-input[1:0]s;
+input [3:0] in;
+input [1:0] s;
 output y;
-wire [1:0]t;
-mux2_1 mux2x1_1(in[1:0],s[0],t[0]);
-mux2_1 mux2x1_2(in[3:2],s[0],t[1]);
-mux2_1 mux2x1_3(t,s[1],y);
+wire [1:0] t;
+// First stage uses s[0]
+mux2_1 mux2_1_1(in[1:0],s[0],t[0]);
+mux2_1 mux2_1_2(in[3:2],s[0],t[1]);
+// Second stage uses s[1]
+mux2_1 mux2_1_3(t,s[1],y);
 endmodule
